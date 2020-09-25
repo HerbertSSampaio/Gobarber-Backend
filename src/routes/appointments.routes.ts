e213b9924785f1660b2ac5c5daa0f0c51e19d19a
HerbertSousa <herberti1405@outlook.com>
2020-09-25 12:44:43 -0300

    initial commit

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,33 @@
+import { Router } from 'express';
+import bodyParser from 'body-parser';
+import { parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+
+const appointmentsRepository = new AppointmentsRepository();
+
+const jsonParser = bodyParser.json();
+
+appointmentsRouter.get('/', (request, response) => {
+    const appointments = appointmentsRepository.all();
+    return response.json(appointments);
+});
+
+appointmentsRouter.post('/', jsonParser, (request, response) => {
+    try {
+        const { provider, date } = request.body;
+
+        const parsedDate = parseISO(date);
+        const createAppointment = new CreateAppointmentService(appointmentsRepository);
+
+        const appointment = createAppointment.execute({ date: parsedDate, provider });
+
+        return response.json(appointment);
+    } catch (err) {
+        return response.status(400).json({ error: err.message });
+    }
+});
+
+export default appointmentsRouter;
