9ad06453519369693aefcd72e43588eb1c13a240
HerbertSousa <herberti1405@outlook.com>
2020-09-28 19:39:51 -0300

    Feature: Model and Migration Users

--- src/models/User.ts
+++ src/models/User.ts
@@ -8,16 +8,19 @@ import {
     UpdateDateColumn,
 } from 'typeorm';
 
-@Entity('appointments')
-class Appointment {
+@Entity('users')
+class User {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
     @Column()
-    provider: string;
+    name: string;
 
-    @Column('timestamp with time zone')
-    date: Date;
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
 
     @CreateDateColumn()
     created_at: Date;
@@ -26,4 +29,4 @@ class Appointment {
     updated_at: Date;
 }
 
-export default Appointment;
+export default User;
