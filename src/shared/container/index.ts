a1b035742d993744205ff289aee263abf52fce24
HerbertSousa <herberti1405@outlook.com>
2021-03-02 10:41:30 -0300

    create unit test to AuthenticateUserService

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,7 +2,7 @@ import { container } from 'tsyringe';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
-
+import '@modules/users/providers';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
