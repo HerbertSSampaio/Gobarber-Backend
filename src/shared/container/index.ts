0f1910c3143c0282509b443cdb4bb0d4c8e1e84b
HerbertSousa <herberti1405@outlook.com>
2021-03-05 13:42:30 -0300

    Save Tokens

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,10 +6,16 @@ import '@modules/users/providers';
 import './providers';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
 container.registerSingleton<IAppointmentsRepository>(
     'AppointmentsRepository',
     AppointmentsRepository,
+);
+container.registerSingleton<IUserTokensRepository>(
+    'UserTokensRepository',
+    UserTokensRepository,
 );
 
 container.registerSingleton<IUsersRepository>(
